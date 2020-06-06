18d76df2cabe95af9f2e9396771e6135d4d73c66
Luke-McShane <lukejsmcshane@gmail.com>
2020-06-06 16:36:34 +1000

    Ternary operator exercise.

---
+++ 07_ternary.js
@@ -0,0 +1,6 @@
+let userAttempt = 'test123';
+let password = 'test321';
+
+// The first part is the expression we are evaluating; the second part is the True statement;
+// the last part is the Fail statement
+userAttempt === password ? console.log('Login Successful!') : console.log('Login Failed!');
