bb40395d349533dbe91998eb11c6f076d8d137d2
Luke-McShane <lukejsmcshane@gmail.com>
2020-11-08 11:01:47 +1100

    Created Section 17 folder with 1st exercise

---
+++ Section 17 Making HTTP Requests/01_ajax_into.js
@@ -0,0 +1,4 @@
+// Ajax requests are carried out asynchronously, meaning that they may be carried out/executed without the webpage having to refresh
+
+// Both XML and JSON are standards/formats of sending/receiving information to/from a server from/to, for example, a web browser
+// AJAX uses one of these formats when it is managing requests, using the format to structure the data being sent
