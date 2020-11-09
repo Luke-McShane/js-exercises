11f8fd4f586d0632e12f884aba93083bc5738537
Luke-McShane <lukejsmcshane@gmail.com>
2020-11-09 16:48:38 +1100

    New section created on Prototypes, etc.

---
+++ Section 19 Prototypes, Classes, and the New Operator/01_prototypes.js
@@ -0,0 +1,5 @@
+// Prototypes are templates/blueprints that all JS objects inherit methods and properties from
+// So, for example, the Array prototype has a select number of methods and properties that every
+// array you create will inherit
+// Every array, for example, will have a __proto__ property, which is a direct link to this prototype
+// object, meaning it has acccess to all the inherited properties and methods
