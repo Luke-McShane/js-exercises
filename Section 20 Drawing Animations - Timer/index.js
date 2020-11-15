137d61a13b56a4bd9659f7e06066df358d7f5241
Luke-McShane <lukejsmcshane@gmail.com>
2020-11-16 05:47:46 +1100

    Basis of project

---
+++ Section 20 Drawing Animations - Timer/index.js
@@ -0,0 +1,24 @@
+class Timer {
+
+  // Each argument will be a DOM element
+  constructor(durationInput, playButton, pauseButton) {
+
+  }
+
+  start() {
+
+  }
+
+  pause() {
+
+  }
+
+  // Call whenever the user changes the duration value
+  onDurationChange() {
+
+  }
+
+  tick() {
+
+  }
+}
