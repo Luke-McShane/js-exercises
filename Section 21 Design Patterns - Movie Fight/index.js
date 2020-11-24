451c74bd5be49012e4ef13822134cfe56b469a4b
Luke-McShane <lukejsmcshane@gmail.com>
2020-11-25 06:06:33 +1100

    Basic Axios setup

--- Section 21 Design Patterns - Movie Fight/index.js
+++ Section 21 Design Patterns - Movie Fight/index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const getMovies = (url) => axios.get(url);
+const printMovies = ({ data }) => {
+  debugger;
+  console.log(data);
+
+  // for (let film of data.result) {
+  //   console.log(film);
+  // }
+}
+
+getMovies('http://www.omdbapi.com/?apikey=4d28104b&t=manhattan')
+  .then((res) => printMovies(res))
+  .catch((err) => console.log(`OH NO: ${err}`));
